7ee0d135814117d9421f9228b090a339332a7dea
Lee <lee@mtftm.com>
2025-01-29 13:26:37 +0800

    use musl

--- toolchain-riscv64.cmake
+++ toolchain-riscv64.cmake
@@ -6,14 +6,14 @@ set(CMAKE_SYSTEM_PROCESSOR riscv64)
 set(tools /opt/riscv)
 
 # 设置 C 和 C++ 编译器
-set(CMAKE_C_COMPILER ${tools}/bin/riscv64-unknown-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/riscv64-unknown-linux-gnu-g++)
+set(CMAKE_C_COMPILER ${tools}/bin/riscv64-unknown-linux-musl-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/riscv64-unknown-linux-musl-g++)
 
 # 设置 sysroot（如果有）
 set(CMAKE_SYSROOT /opt/riscv/sysroot)
 
 # 配置查找路径
-set(CMAKE_FIND_ROOT_PATH ${tools}/riscv64-unknown-linux-gnu/sysroot)
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
